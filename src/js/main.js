6eaaed963494bd8bea51c143568975a86deffa79
Mari <navarro.ortiz.maricarmen@gmail.com>
2026-07-02 21:52:50 +0200

    add js structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+//SECCIÓN DE QUERY-SELECTORS
+
+//SECCIÓN DE DATOS
+let artists = [];
+
+//SECCIÓN DE FUNCIONES
+function loadArtists() {}
+
+function renderArtists() {}
+
+function saveLocalStorage() {}
+
+function handleClickFollow() {}
+//SECCIÓN DE FUNCIONES DE EVENTOS
+
+//SECCIÓN DE EVENTOS
+
+//SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
+
+function init() {
+  loadArtists();
+}
+
+init();
+
+console.log(">> Ready :)");
